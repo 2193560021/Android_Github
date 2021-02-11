4d9b5191e7058587f4873ba612496139a748a174
2193560021 <62364555+2193560021@users.noreply.github.com>
2021-02-11 14:36:24 +0800

    服务

---
+++ ServiceTest/MyService.java
@@ -0,0 +1,63 @@
+package com.example.servicetest;
+
+import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+public class MyService extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+    class DownloadBinder extends Binder{
+        public void starDownload(){
+            Log.d("MyService", "开始下载启动");
+        }
+        public int getProgress(){
+            Log.d("MyService", "得到进度");
+            return 0;
+        }
+    }
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate已启动");
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("标题")
+                .setContentText("内容")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1,notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand已启动");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy已启动");
+    }
+}
